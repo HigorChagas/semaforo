cb5018f0ca53b8b779ba1c2c301c0f2ba8b044b0
Higor Chagas <higornchagas@gmail.com>
2021-12-27 09:29:39 -0300

    Implementado função que deixa o semaforo aleatório

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function semaforo() {
     const img = document.querySelector('#img');  
 
     let colorIndex;
+    let random = null;
 
     const nextIndex = () => {
         if(colorIndex < 2) {
@@ -11,30 +12,43 @@ function semaforo() {
         } else {
             colorIndex = 0;
         }
+         
+        return colorIndex;
     } 
+
+    
     
     buttons.addEventListener('click', (e) => {
         const elementos = e.target;
         if(elementos.classList.contains('button-green')) {
             ligar.green();
+            clearInterval(random);
         }
         if(elementos.classList.contains('button-yellow')) {
             ligar.yellow();
+            clearInterval(random);
         }
         if(elementos.classList.contains('button-red')) {
             ligar.red();
+            clearInterval(random);
         }
         if(elementos.classList.contains('button-random')) {
-            console.log(ligar[nextIndex()]);
-            
+            clearInterval(random);
+            random = setInterval(() => {
+                currentColor = Object.keys(ligar)[nextIndex()];
+                    ligar[currentColor]();
+            }, 1000);
+
+            random;
         }
     }); 
 
-    const ligar = {
+    const ligar = { 
         'red': () => img.src = './img/vermelho.png',
         'yellow': () => img.src = './img/amarelo.png',
-        'green': () => img.src = './img/verde.png'
+        'green': () => img.src = './img/verde.png' 
     }
+
 }
 
 semaforo();
